a0d09fadd53798da01dad1369a8013da495057fb
Alcoh Nguyen <Alcoh174@gmail.com>
2024-09-18 10:47:56 +0700

    Update Topic_02_Primitive_Reference.java

--- selenium/webdriver/Topic_02_Primitive_Reference.java
+++ selenium/webdriver/Topic_02_Primitive_Reference.java
@@ -3,22 +3,22 @@ package javaTester;
 public class Topic_02_Primitive_Reference{
     int x;
     public static void main(String[] args){
-        int x = 15;
+        int x = 10;
         int y = x;
         System.out.println("x = " + x);
         System.out.println("y = " + y);
-        x = 30;
+        x = 20;
         System.out.println("x = " + x);
         System.out.println("y = " + y);
         // Class
         // Instance 1
         Topic_02_Primitive_Reference c = new Topic_02_Primitive_Reference();
-        c.x = 43;
+        c.x = 40;
         System.out.println(c.x);
         // Instance 2
         Topic_02_Primitive_Reference d = new Topic_02_Primitive_Reference();
         System.out.println(d.x);
-        c.x = 16;
+        c.x = 30
         d = c;
         System.out.println(c.x);
         System.out.println(d.x);
